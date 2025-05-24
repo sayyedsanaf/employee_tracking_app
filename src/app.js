a1bfd94a4783285a714d7a4ad4240a7c0984139a
sanaf3739 <sanaf6350@gmail.com>
2025-05-24 14:57:22 +0530

    configured server for vercel

--- src/app.js
+++ src/app.js
@@ -1,16 +1,43 @@
-import express from 'express';
-import { errorHandler } from './middlewares/error.middleware.js';
-import authRoutes from './routes/auth.route.js';
+// src/createServer.js
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./utils/db.js";
+
+// Routes
+import authRoutes from "./routes/auth.routes.js";
+import employeeRoutes from "./routes/employee.routes.js";
+import locationRoutes from "./routes/location.routes.js";
+import attendanceRoutes from "./routes/attendance.routes.js";
+import devRoutes from "./routes/dev.routes.js";
+import visitRoutes from "./routes/visit.routes.js";
+
+// Middlewares
+import errorHandler from "./middlewares/error.middleware.js";
+
+// Cron Jobs
+import "./jobs/attendanceScheduler.js";
+
+// Config
+dotenv.config();
+connectDB();
 
 const app = express();
 
+// Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/locations", locationRoutes);
+app.use("/api/attendance", attendanceRoutes);
+app.use("/api/visit", visitRoutes);
+app.use("/api/dev", devRoutes);
 
-// Global Error Handler
+// Error handler
 app.use(errorHandler);
 
 export default app;
