eb56ebb3629e0882bbba1aa739bb806f4cbea645
sanaf3739 <sanaf6350@gmail.com>
2025-05-24 19:53:12 +0530

    fixed db issue for vercel

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,14 @@
 // utils/db.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+let isConnected;
 
 const connectDB = async () => {
+  if (isConnected) return;
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    isConnected = conn.connections[0].readyState;
+    console.log(isConnected)
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
